c3bb18ff6dcb5fd300093802df795ab5943fb88f
Orion Dollar <oriondollar@Orions-MacBook-Pro.local>
2020-03-06 14:04:08 -0800

    data_loader module

---
+++ se3cnn_v3/util/data_loader.py
@@ -0,0 +1,9 @@
+import torch
+import os
+import numpy as np
+from scipy.stats import special_ortho_group
+
+def voxelizer(pdf_fn):
+    # Code that gets n_atoms, positions, atom_types and residue arrays from pdb
+
+    return 0
