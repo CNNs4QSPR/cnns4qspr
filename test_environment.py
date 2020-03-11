a79b24d410fa46df5c56a455773a3639c9e055dd
Dave Juergens <davidcj@uw.edu>
2020-03-11 12:52:50 -0700

    Testing the travis yml script, added an echo statement to see what happens

--- test_environment.py
+++ test_environment.py
@@ -5,14 +5,20 @@ from cnns4qspr.loader import *
 ### Create input tensor
 test_pdb_fn = 'cnns4qspr/formatting_data/sample_pdbs/1a00B00'
 pdb_dict = load_pdb(test_pdb_fn)
-field = voxelize(pdb_dict)['CA']
+print(pdb_dict['atom_type_set'])
+afield = make_fields(pdb_dict)
+print(afield)
+
+field = voxelize(test_pdb_fn, channels=['CA', 'CD', 'NH2'])
+print(field.keys())
 # x = torch.autograd.Variable(field, volatile=True)
 
 ### Load model
-cnn = load_cnn('cnn_no_vae.ckpt')
+#cnn = load_cnn('cnn_no_vae.ckpt')
 
 ### Internals plots
 plot_field(field, threshold=0.0001)
 plot_internals(cnn, field, 0, 0, threshold_on=False)
 plot_internals(cnn, field, 0, 2, threshold_on=False)
 plot_internals(cnn, field, 0, 3, threshold_on=False)
+#plot_internals(cnn, field, 0, 0)
