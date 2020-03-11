7aaf5f18e64b23e307ca61db6611a5a50bfc4889
Nisarg Joshi <nisargj@uw.edu>
2020-03-11 16:46:02 -0700

    finished writing the unit tests for loader. Works fine

--- cnns4qspr/tests/test_loader.py
+++ cnns4qspr/tests/test_loader.py
@@ -1,12 +1,15 @@
 import torch
 import numpy as np
 from biopandas.pdb import PandasPdb
-import unittest
+# import unittest
 import sys
 sys.path.insert(1,'/Users/nisargjoshi/Desktop/direct_project/cnns4qspr/cnns4qspr')
 import loader
 
 path = '/Users/nisargjoshi/Desktop/direct_project/cnns4qspr/cnns4qspr/formatting_data/sample_pdbs/1a00B00'
+protein_dict = loader.load_pdb(path)
+num_bins = 50
+bin_size = 2
 
 def test_load_pdb():
     """
@@ -34,4 +37,32 @@ def test_voxelize():
     protein_dict = loader.load_pdb(path)
     dfields = loader.voxelize(path)
     assert dfields['CA'].shape == torch.Size([1, 1, 50, 50, 50])
+    assert dfields['CA'].dtype == torch.float32
+    # assert dfields['CA'].device == torch.device('cpu')
     return dfields
+
+def test_grid_positions():
+    """
+    This unit test is for testing the grid_positions function.
+    """
+
+    grid_array = torch.linspace(start=-num_bins / 2 * bin_size + bin_size / 2,
+                             end=num_bins / 2 * bin_size - bin_size / 2,
+                             steps=num_bins)
+    grid_out = loader.grid_positions(grid_array)
+    assert len(grid_out[0]) == 50
+    assert len(grid_out[1]) == 50
+    assert len(grid_out[2]) == 50
+    assert grid_out[0].shape == torch.Size([50, 50, 50])
+    assert grid_out[1].shape == torch.Size([50, 50, 50])
+    assert grid_out[2].shape == torch.Size([50, 50, 50])
+    return grid_out
+
+def test_make_fields():
+    """
+    This is for testing the make_fields function.
+    """
+    output_fields = loader.make_fields(protein_dict)
+    assert output_fields['CA'].shape == torch.Size([1, 1, 50, 50, 50])
+    assert output_fields['CA'].dtype == torch.float32
+    return output_fields
