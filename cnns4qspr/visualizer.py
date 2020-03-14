42d1a0feafb0d9c0fb66ab88c6203505013c9794
Saransh <saranshj@uw.edu>
2020-03-14 13:42:08 -0700

    Made the file pep8/pycodestyle compliant

--- cnns4qspr/visualizer.py
+++ cnns4qspr/visualizer.py
@@ -8,28 +8,39 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+
 def outer_block1_hook(module, input_, output):
     global outer_block1_out
     outer_block1_out = output
+
 
 def outer_block2_hook(module, input_, output):
     global outer_block2_out
     outer_block2_out = output
 
+
 def outer_block3_hook(module, input_, output):
     global outer_block3_out
     outer_block3_out = output
 
+
 def outer_block4_hook(module, input_, output):
     global outer_block4_out
     outer_block4_out = output
+
 
 def outer_block5_hook(module, input_, output):
     global outer_block5_out
     outer_block5_out = output
 
 
-def plot_field(field, color='ice_r', from_voxelizer=True, threshold=0.2, alpha=0.7, show=True):
+def plot_field(
+        field,
+        color='ice_r',
+        from_voxelizer=True,
+        threshold=0.2,
+        alpha=0.7,
+        show=True):
     """
     This function takes a field datacube from voxelizer and plots a heat map of
     the 50x50x50 field. The field describes an atomic "denisty" at each voxel.
@@ -74,16 +85,17 @@ def plot_field(field, color='ice_r', from_voxelizer=True, threshold=0.2, alpha=0
                         color='intensity', opacity=alpha,
                         color_continuous_scale=color)
     fig.update_layout(
-        scene = dict(
-            xaxis = dict(range=[-25, 25]),
-            yaxis = dict(range=[-25, 25]),
-            zaxis = dict(range=[-25, 25])
+        scene=dict(
+            xaxis=dict(range=[-25, 25]),
+            yaxis=dict(range=[-25, 25]),
+            zaxis=dict(range=[-25, 25])
         )
     )
     if show:
         fig.show()
     else:
         return fig
+
 
 def plot_internals(model, field, block=0, channel=0, threshold_on=True):
     """
@@ -106,7 +118,7 @@ def plot_internals(model, field, block=0, channel=0, threshold_on=True):
               outer_block4_out,
               outer_block5_out]
 
-    internal_field = blocks[block][:,channel, :, :, :].detach()
+    internal_field = blocks[block][:, channel, :, :, :].detach()
     if threshold_on:
         fig = plot_field(internal_field, show=False)
     else:
