d45ea877bbb8e56a325de64e018b2654cc561b32
Dave Juergens <davidcj@uw.edu>
2020-03-14 19:49:08 -0700

    Added some tests to the test_featurizer code

--- cnns4qspr/test/test_featurizer.py
+++ cnns4qspr/test/test_featurizer.py
@@ -12,16 +12,32 @@ class test_featurizer(unittest.TestCase):
         """
         This method tests the load_cnn function
         """
-        path = 'cnns4qspr/formatting_data/sample_pdbs/6fww.pdb'
+        # make sure the model can actually load
+        cnn = featurizer.load_cnn('cnn_no_vae.ckpt')
+        self.assertTrue(cnn != None)
 
 
     def test_featurize(self):
         """
         This method tests the featurizer method
         """
+        path = 'cnns4qspr/formatting_data/sample_pdbs/6fww.pdb'
+        fields = loader.voxelize(path, channels=['LYS'])
+        features = featurizer.featurize(fields)
+
+        # make sure the two dictionaries contain same
+        # number of entries
+        self.assertTrue(len(fields.keys()) == len(features.keys()))
+        for key in features:
+            self.assertTrue(len(features[key][0]) == 256)
+
+
 
 
     def test_gen_feature_set(self):
         """
         This method tests the gen_feature_set function
         """
+
+    if __name__ == '__main__':
+        unittest.main()
