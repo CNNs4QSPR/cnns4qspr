09fac47473db9a24d135870b2fd08737d8b7e14c
Saransh <saranshj@uw.edu>
2020-03-18 17:02:36 -0700

    Made a setup file.

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='cnns4qspr',
+	description='A package for creating rich, equivariant, structural and chemical features from protein structure data.',
+    	url='https://https://github.com/CNNs4QSPR/cnns4qspr',
+    	author=[
+		'Orion Dollar',
+		'David Juergens',
+		'Nisarg Joshi',
+		'Saransh Jain'
+		],
+    	author_email=[
+		'orion.dollar@gmail.com',
+		'davidcj@uw.edu',
+		'nisargj@uw.edu',
+		'saranshj$uw.edu',
+		]
+    	license='MIT License',
+   	packages=find_packages(),
+    	install_requires=[
+			'numpy',
+			'scipy'
+			'torch',
+			'pandas',
+			'plotly.express',
+			'biopandas',
+			'os',
+			'sys',
+			'pkg_resources'
+			'lie_learn'
+			'se3cnn'
+        		],
+
+    	classifiers=[
+        	'Intended Audience :: Science/Research',
+       		'License :: OSI Approved :: MIT License',
+        	'Operating System :: POSIX :: Linux',
+        	'Programming Language :: Python :: 3.7',
+    		],
+)
